16fc4fed88b2d43f9462660b46395e5244e8fac9
zani-t <tembo4.t@gmail.com>
2023-07-23 19:03:45 -0400

    1.4 protocol and triple hello world test

--- client.cpp
+++ client.cpp
@@ -1,23 +1,108 @@
+// #include "utils.hpp"
 #include <iostream>
+#include <cassert>
+#include <cstdlib>
 #include <cstring>
 
+#include <stdint.h>         // ...
 #include <errno.h>          // Error handling
 #include <unistd.h>         // Read & write functions
 #include <arpa/inet.h>      // Internet operations
+#include <sys/socket.h>     // ...
+#include <netinet/ip.h>     // ...
 
-// DEFINE IN HEADER/SEPARATE FILE
+// EXPORT
+const size_t k_max_msg = 4096;
+
+static void msg(const char *msg) {
+    fprintf(stderr, "%s\n", msg);
+}
+
 static void die(const char *msg) {
     int err = errno;
     fprintf(stderr, "[%d], %s\n", err, msg);
     abort();
 }
 
+// Read n bytes from kernel [through several requests]
+static int32_t read_full(int fd, char *buf, size_t n) {
+    while (n > 0) {
+        ssize_t rv = read(fd, buf, n);
+        // Error or unexpected EOF
+        if (rv <= 0)
+            return -1;
+
+        assert((size_t)rv <= n);
+        n -= (size_t)rv;
+        buf += rv;
+    }
+    return 0;
+}
+
+// Write n bytes [through several requests]
+static int32_t write_all(int fd, const char *buf, size_t n) {
+    while (n > 0) {
+        ssize_t rv = write(fd, buf, n);
+        if (rv <= 0)
+            return -1;
+        
+        assert((size_t)rv <= n);
+        n -= (size_t)rv;
+        buf += rv;
+    }
+    return 0;
+}
+
+static int32_t query(int fd, const char *text) {
+    uint32_t len = (uint32_t)strlen(text);
+    if (len > k_max_msg)
+        return -1;
+    
+    // Create header & copy request body
+    char wbuf[4 + k_max_msg];
+    memcpy(wbuf, &len, 4);
+    memcpy(&wbuf[4], text, len);
+    if (int32_t err = write_all(fd, wbuf, 4 + len))
+        return err;
+    
+    // Get header value
+    char rbuf[4 + k_max_msg + 1];
+    errno = 0;
+    int32_t err = read_full(fd, rbuf, 4);
+    if (err) {
+        if (errno == 0)
+            msg("EOF");
+        else
+            msg("read() error (head)");
+        return err;
+    }
+
+    // Set header
+    memcpy(&len, rbuf, 4);
+    if (len > k_max_msg) {
+        msg("too long");
+        return -1;
+    }
+
+    // Read request body
+    err = read_full(fd, &rbuf[4], len);
+    if (err) {
+        msg("read() error (body)");
+        return err;
+    }
+
+    // Action - read request
+    rbuf[4 + len] = '\0';
+    printf("server says: %s\n", &rbuf[4]);
+    return 0;
+}
+
 int main() {
     // Retrieve file desciptor
     int fd = socket(AF_INET, SOCK_STREAM, 0);
     if (fd < 0)
-        die("socket()");    
-    
+        die("socket()");
+
     // Bind to IPv4 address
     struct sockaddr_in addr = {};
     addr.sin_family = AF_INET;
@@ -28,18 +113,18 @@ int main() {
     if (rv)
         die("connect");
     
-    // Action - write message
-    char msg[] = "hello";
-    write(fd, msg, strlen(msg));
+    // Action - send three requests
+    int32_t err = query(fd, "hello1");
+    if (err)
+        goto L_DONE;
+    err = query(fd, "hello2");
+    if (err)
+        goto L_DONE;
+    err = query(fd, "hello3");
+    if (err)
+        goto L_DONE;
 
-    // Get response
-    char rbuf[64] = {};
-    ssize_t n = read(fd, rbuf, sizeof(rbuf) - 1);
-    if (n < 0)
-        die("read");
-    
-    printf("server says: %s\n", rbuf);
+    L_DONE:
     close(fd);
-
     return 0;
 }
--- server.cpp
+++ server.cpp
@@ -1,4 +1,6 @@
+// #include "utils.hpp"
 #include <iostream>
+#include <cassert>
 #include <cstdlib>
 #include <cstring>
 
@@ -8,6 +10,9 @@
 #include <arpa/inet.h>      // Internet operations
 #include <sys/socket.h>     // ...
 #include <netinet/ip.h>     // ...
+
+// EXPORT
+const size_t k_max_msg = 4096;
 
 static void msg(const char *msg) {
     fprintf(stderr, "%s\n", msg);
@@ -19,24 +24,85 @@ static void die(const char *msg) {
     abort();
 }
 
-// Connection test
-static void do_something(int connfd) {
-    char rbuf[64] = {};
-    ssize_t n = read(connfd, rbuf, sizeof(rbuf) - 1);
-    if (n < 0) {
-        msg("read() error");
-        return;
+// Read n bytes from kernel [through several requests]
+static int32_t read_full(int fd, char *buf, size_t n) {
+    while (n > 0) {
+        ssize_t rv = read(fd, buf, n);
+        // Error or unexpected EOF
+        if (rv <= 0)
+            return -1;
+
+        assert((size_t)rv <= n);
+        n -= (size_t)rv;
+        buf += rv;
+    }
+    return 0;
+}
+
+// Write n bytes [through several requests]
+static int32_t write_all(int fd, const char *buf, size_t n) {
+    while (n > 0) {
+        ssize_t rv = write(fd, buf, n);
+        if (rv <= 0)
+            return -1;
+        
+        assert((size_t)rv <= n);
+        n -= (size_t)rv;
+        buf += rv;
+    }
+    return 0;
+}
+
+/*  In order to process requests separately we create a request protocol
+    of a 4-byte header containing the message length, followed by the
+    variable-length message itself.  */
+static int32_t one_request(int connfd) {
+    char rbuf[4 + k_max_msg + 1];
+    errno = 0;
+    int32_t err = read_full(connfd, rbuf, 4);
+    if (err) {
+        if (errno == 0)
+            msg("EOF");
+        else
+            msg("read() error (head)");
+        return err;
     }
 
-    printf("client says: %s\n", rbuf);
-    char wbuf[] = "world";
-    write(connfd, wbuf, strlen(wbuf));
+    // Get header describing length [little endian]
+    // memcpy after error handling?
+    uint32_t len = 0;
+    memcpy(&len, rbuf, 4);
+    if (len > k_max_msg) {
+        msg("too long");
+        return -1;
+    }
+
+    // Read request body
+    err = read_full(connfd, &rbuf[4], len);
+    if (err) {
+        msg("read() error (body)");
+        return err;
+    }
+
+    // Action - show request
+    rbuf[4 + len] = '\0';
+    printf("client says: %s\n", &rbuf[4]);
+
+    // Reply using same protocol
+    const char reply[] = "world";
+    char wbuf[4 + sizeof(reply)];
+    len = (uint32_t)strlen(reply);
+    memcpy(wbuf, &len, 4);
+    memcpy(&wbuf[4], reply, len);
+    return write_all(connfd, wbuf, 4 + len);
 }
 
 int main() {
     // Retrieve file desciptor - handler for given type of i/o resource
     // Parameters [assert] IPv4 TCP socket
     int fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (fd < 0)
+        die("socket()");
     
     // Introduce syscall,, params configure socket
     int val = 1;
@@ -68,8 +134,12 @@ int main() {
         if (connfd < 0)
             continue;
 
-        // Action - respond to messages
-        do_something(connfd);
+        // Handle one request until connection is lost
+        while (true) {
+            int32_t err = one_request(connfd);
+            if (err)
+                break;
+        }
         // Recycle fd
         close(connfd);
     }
